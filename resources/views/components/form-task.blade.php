cffb6b4c5aafb545b388d58f1a74cbbfd84f9e10
irvyn2703 <tosolatoso17@gmail.com>
2025-02-11 15:57:49 -0600

    feat(): create formTask component

---
+++ resources/views/components/form-task.blade.php
@@ -0,0 +1,39 @@
+<form action="{{ $action }}" method="POST" class="row">
+    @csrf
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 mt-2">
+            <div class="form-group">
+                <strong>Tarea:</strong>
+                <input type="text" name="title" class="form-control" placeholder="Tarea" required>
+                @error('title')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 mt-2">
+            <div class="form-group">
+                <strong>Descripción:</strong>
+                <textarea class="form-control" style="height:150px" name="description" placeholder="Descripción..." required></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 mt-2">
+            <div class="form-group">
+                <strong>Fecha límite:</strong>
+                <input type="date" name="due_date" class="form-control" id="">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 mt-2">
+            <div class="form-group">
+                <strong>Estado (inicial):</strong>
+                <select name="status" class="form-select" id="" required>
+                    <option value="pendiente">Pendiente</option>
+                    <option value="en progreso">En progreso</option>
+                    <option value="completada">Completada</option>
+                </select>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center mt-2">
+            <button type="submit" class="btn btn-primary w-100 mb-5 fs-3 fw-bold">Crear</button>
+        </div>
+    </div>
+</form>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,58 +1,20 @@
 @extends('layouts.base')
 
 @section('content')
-<div class="col-12 d-flex align-items-center flex-column">
-    <div>
-        <h2 class="text-white p-3">CRUD de Tareas</h2>
-    </div>
-</div>
-
 @if (session()->has('success'))
     <div class="alert alert-success mt-3 row">
         <strong>{{ session('success') }}</strong>
     </div>
 @endif
 
-
-<form action="{{route('store')}}" method="POST" class="row">
-    @csrf
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12 mt-2">
-            <div class="form-group">
-                <strong>Tarea:</strong>
-                <input type="text" name="title" class="form-control" placeholder="Tarea" required>
-                @error('title')
-                    <span class="text-danger">{{ $message }}</span>
-                @enderror
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 mt-2">
-            <div class="form-group">
-                <strong>Descripción:</strong>
-                <textarea class="form-control" style="height:150px" name="description" placeholder="Descripción..." required></textarea>
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-6 mt-2">
-            <div class="form-group">
-                <strong>Fecha límite:</strong>
-                <input type="date" name="due_date" class="form-control" id="">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-6 mt-2">
-            <div class="form-group">
-                <strong>Estado (inicial):</strong>
-                <select name="status" class="form-select" id="" required>
-                    <option value="pendiente">Pendiente</option>
-                    <option value="en progreso">En progreso</option>
-                    <option value="completada">Completada</option>
-                </select>
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center mt-2">
-            <button type="submit" class="btn btn-primary w-100 mb-5 fs-3 fw-bold">Crear</button>
-        </div>
+<div class="col-12 d-flex align-items-center flex-column">
+    <div>
+        <h2 class="text-white p-3">CRUD de Tareas</h2>
     </div>
-</form>
+</div>
+
+<x-form-task :action="route('store')" />
+
 
 <div class="row">
     <div class="col-12 mt-4">
